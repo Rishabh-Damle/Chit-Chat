c54e806d2fa0d141ea86d160925c6d7a59c034c1
Vrushabh Damle <dwrushabh@gmail.com>
2025-11-08 13:49:03 +0530

    some modifications

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,8 +4,9 @@ import "./App.css";
 function App() {
   const [messages, setMessages] = useState(["hi there", "hello"]);
 
-  const wsRef = useRef<WebSocket | null>(null);
+  const wsRef = useRef<WebSocket>(null);
   const inputRef = useRef<HTMLInputElement>(null);
+  const messagesEndRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     const ws = new WebSocket("http://localhost:8080");
     ws.onmessage = (event) => {
@@ -26,18 +27,26 @@ function App() {
       ws.close;
     };
   }, []);
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
   return (
-    <div className="h-screen bg-neutral-950 flex flex-col justify-between flex-wrap">
-      <div className="mt-8 ">
+    <div className="h-screen bg-neutral-950 flex flex-col justify-between">
+      <div className="mt-8 overflow-y-auto">
         {messages.map((message) => (
-          <div className="m-10">
+          <div className="m-10 ">
             <span className="bg-cyan-100 p-4 m-8 rounded">{message} </span>
           </div>
         ))}
+        <div ref={messagesEndRef} />
       </div>
-      <div className="bg-neutral-300 w-full flex ">
+      <div className="bg-cyan-100 w-full flex fixed bottom-0">
         {" "}
-        <input ref={inputRef} className="flex-1 p-4 outline-none border-none" />
+        <input
+          ref={inputRef}
+          className="flex-1 p-4 outline-none border-none bg-transparent"
+          placeholder="Type a message..."
+        />
         <button
           className="bg-green-600 text-neutral-50 p-4 cursor-pointer outline-none border-none"
           onClick={() => {
